7bbbe1c4563ab1f1c79c307eea0a272a1c61b2a3
baked <tristancilltseinleben@gmail.com>
2023-07-06 18:35:22 +0200

    initial commit from create-turbo-module

---
+++ packages/eslint-config-custom/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "turbo",
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+};
