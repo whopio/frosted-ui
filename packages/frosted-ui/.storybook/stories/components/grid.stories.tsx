ac9b1ab27ea0574479ed76554516e393ff7b495e
Artur Bien <artur.bien@gmail.com>
2023-10-16 12:19:21 +0200

    Layout components

---
+++ packages/frosted-ui/.storybook/stories/components/grid.stories.tsx
@@ -0,0 +1,94 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import React from 'react';
+import { Box } from '../../../src/components/box';
+import { Grid } from '../../../src/components/grid';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: 'Layout/Grid',
+  component: Grid,
+  args: {
+    width: 'auto',
+    columns: '3',
+    // rows: '2',
+    gapX: '3',
+    gapY: '6',
+  },
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
+  render: (args) => (
+    <Grid {...args}>
+      <Box width="9" height="9" shrink="0">
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            background:
+              'repeating-linear-gradient( 45deg, var(--orange-8), var(--orange-8) 5px, var(--orange-6) 5px, var(--orange-6) 25px )',
+          }}
+        ></div>
+      </Box>
+      <Box width="9" height="9" shrink="0">
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            background:
+              'repeating-linear-gradient( 45deg, var(--orange-8), var(--orange-8) 5px, var(--orange-6) 5px, var(--orange-6) 25px )',
+          }}
+        ></div>
+      </Box>
+      <Box width="9" height="9" shrink="0">
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            background:
+              'repeating-linear-gradient( 45deg, var(--orange-8), var(--orange-8) 5px, var(--orange-6) 5px, var(--orange-6) 25px )',
+          }}
+        ></div>
+      </Box>
+      <Box width="9" height="9" shrink="0">
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            background:
+              'repeating-linear-gradient( 45deg, var(--orange-8), var(--orange-8) 5px, var(--orange-6) 5px, var(--orange-6) 25px )',
+          }}
+        ></div>
+      </Box>
+      <Box width="9" height="9" shrink="0">
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            background:
+              'repeating-linear-gradient( 45deg, var(--orange-8), var(--orange-8) 5px, var(--orange-6) 5px, var(--orange-6) 25px )',
+          }}
+        ></div>
+      </Box>
+      <Box width="9" height="9" shrink="0">
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            background:
+              'repeating-linear-gradient( 45deg, var(--orange-8), var(--orange-8) 5px, var(--orange-6) 5px, var(--orange-6) 25px )',
+          }}
+        ></div>
+      </Box>
+    </Grid>
+  ),
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+} satisfies Meta<typeof Grid>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: Story = {};
