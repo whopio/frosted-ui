f87f85d76d602b7bf888c6648fd5b060f871119e
Artur Bien <artur.bien@gmail.com>
2023-10-16 19:15:13 +0200

    AlertDialog

--- packages/frosted-ui/.storybook/stories/components/alert-dialog.stories.tsx
+++ packages/frosted-ui/.storybook/stories/components/alert-dialog.stories.tsx
@@ -1,46 +1,127 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
 
-import { AlertDialog } from '../../../src/components/alert-dialog';
+import {
+  AlertDialog,
+  Button,
+  Code,
+  Flex,
+  Inset,
+  Table,
+  Text,
+} from '../../../src/components';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: 'Components/AlertDialog',
   component: AlertDialog.Root,
   args: {},
+
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
-  render: ({ children, ...args }) => (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <button color="red">Revoke access</button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content style={{ maxWidth: 450 }}>
-        <AlertDialog.Title>Revoke access</AlertDialog.Title>
-        <AlertDialog.Description size="2">
-          Are you sure? This application will no longer be accessible and any
-          existing sessions will be expired.
-        </AlertDialog.Description>
-
-        <div style={{ display: 'flex', gap: 32 }}>
-          <AlertDialog.Cancel>
-            <button>Cancel</button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <button>Revoke access</button>
-          </AlertDialog.Action>
-        </div>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
-  ),
 } satisfies Meta<typeof AlertDialog.Root>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Default: Story = {};
+export const Default: Story = {
+  render: ({ children, ...args }) => (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button variant="classic" color="red">
+          Revoke access
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content style={{ maxWidth: 450 }}>
+        <AlertDialog.Title mb="3">Revoke access</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Are you sure? This application will no longer be accessible and any
+          existing sessions will be expired.
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Revoke access
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  ),
+};
+
+export const InsetContent: Story = {
+  name: 'With inset content',
+  render: ({ children, ...args }) => (
+    <Flex direction="column" align={'center'} gap="6">
+      <Text>
+        Use the <Code>Inset</Code> component to align content flush with the
+        sides of the dialog (bypassing its padding).
+      </Text>
+      <div>
+        <AlertDialog.Root>
+          <AlertDialog.Trigger>
+            <Button color="red" variant="classic">
+              Delete users
+            </Button>
+          </AlertDialog.Trigger>
+          <AlertDialog.Content style={{ maxWidth: 500 }}>
+            <AlertDialog.Title>Delete Users</AlertDialog.Title>
+            <AlertDialog.Description size="2">
+              Are you sure you want to delete these users? This action is
+              permanent and cannot be undone.
+            </AlertDialog.Description>
+
+            <Inset side="x" my="5">
+              <Table.Root>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.ColumnHeaderCell>Full name</Table.ColumnHeaderCell>
+                    <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
+                    <Table.ColumnHeaderCell>Group</Table.ColumnHeaderCell>
+                  </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                  <Table.Row>
+                    <Table.RowHeaderCell>Danilo Sousa</Table.RowHeaderCell>
+                    <Table.Cell>danilo@example.com</Table.Cell>
+                    <Table.Cell>Developer</Table.Cell>
+                  </Table.Row>
+
+                  <Table.Row>
+                    <Table.RowHeaderCell>Zahra Ambessa</Table.RowHeaderCell>
+                    <Table.Cell>zahra@example.com</Table.Cell>
+                    <Table.Cell>Admin</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              </Table.Root>
+            </Inset>
+
+            <Flex gap="3" justify="end">
+              <AlertDialog.Cancel>
+                <Button variant="soft" color="gray">
+                  Cancel
+                </Button>
+              </AlertDialog.Cancel>
+              <AlertDialog.Action>
+                <Button color="red">Delete users</Button>
+              </AlertDialog.Action>
+            </Flex>
+          </AlertDialog.Content>
+        </AlertDialog.Root>
+      </div>
+    </Flex>
+  ),
+};
--- packages/frosted-ui/src/components/alert-dialog.tsx
+++ packages/frosted-ui/src/components/alert-dialog.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import * as React from 'react';
-import classNames from 'classnames';
 import * as AlertDialogPrimitive from '@radix-ui/react-alert-dialog';
-import { alertDialogContentPropDefs } from './alert-dialog.props';
+import classNames from 'classnames';
+import * as React from 'react';
 import { withBreakpoints } from '../helpers';
+import { Theme } from '../theme';
+import { alertDialogContentPropDefs } from './alert-dialog.props';
 import { Heading } from './heading';
 import { Text } from './text';
-import { Theme } from '../theme';
 
 import type { ExtractPropsForTag, GetPropDefTypes } from '../helpers';
 
@@ -18,61 +18,80 @@ const AlertDialogRoot: React.FC<AlertDialogRootProps> = (props) => (
 );
 AlertDialogRoot.displayName = 'AlertDialogRoot';
 
-type AlertDialogTriggerElement = React.ElementRef<typeof AlertDialogPrimitive.Trigger>;
+type AlertDialogTriggerElement = React.ElementRef<
+  typeof AlertDialogPrimitive.Trigger
+>;
 interface AlertDialogTriggerProps
-  extends Omit<React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Trigger>, 'asChild'> {}
-const AlertDialogTrigger = React.forwardRef<AlertDialogTriggerElement, AlertDialogTriggerProps>(
-  (props, forwardedRef) => <AlertDialogPrimitive.Trigger {...props} ref={forwardedRef} asChild />
-);
+  extends Omit<
+    React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Trigger>,
+    'asChild'
+  > {}
+const AlertDialogTrigger = React.forwardRef<
+  AlertDialogTriggerElement,
+  AlertDialogTriggerProps
+>((props, forwardedRef) => (
+  <AlertDialogPrimitive.Trigger {...props} ref={forwardedRef} asChild />
+));
 AlertDialogTrigger.displayName = 'AlertDialogTrigger';
 
-type AlertDialogContentElement = React.ElementRef<typeof AlertDialogPrimitive.Content>;
-type AlertDialogContentOwnProps = GetPropDefTypes<typeof alertDialogContentPropDefs>;
+type AlertDialogContentElement = React.ElementRef<
+  typeof AlertDialogPrimitive.Content
+>;
+type AlertDialogContentOwnProps = GetPropDefTypes<
+  typeof alertDialogContentPropDefs
+>;
 interface AlertDialogContentProps
-  extends Omit<React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Content>, 'asChild'>,
+  extends Omit<
+      React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Content>,
+      'asChild'
+    >,
     AlertDialogContentOwnProps {
-  container?: React.ComponentProps<typeof AlertDialogPrimitive.Portal>['container'];
+  container?: React.ComponentProps<
+    typeof AlertDialogPrimitive.Portal
+  >['container'];
 }
-const AlertDialogContent = React.forwardRef<AlertDialogContentElement, AlertDialogContentProps>(
-  (props, forwardedRef) => {
-    const {
-      className,
-      forceMount,
-      container,
-      size = alertDialogContentPropDefs.size.default,
-      ...contentProps
-    } = props;
-    return (
-      <AlertDialogPrimitive.Portal container={container} forceMount={forceMount}>
-        <Theme asChild>
-          <AlertDialogPrimitive.Overlay className="rt-DialogOverlay rt-AlertDialogOverlay">
-            <AlertDialogPrimitive.Content
-              {...contentProps}
-              ref={forwardedRef}
-              className={classNames(
-                'rt-DialogContent',
-                'rt-AlertDialogContent',
-                className,
-                withBreakpoints(size, 'rt-r-size')
-              )}
-            />
-          </AlertDialogPrimitive.Overlay>
-        </Theme>
-      </AlertDialogPrimitive.Portal>
-    );
-  }
-);
+const AlertDialogContent = React.forwardRef<
+  AlertDialogContentElement,
+  AlertDialogContentProps
+>((props, forwardedRef) => {
+  const {
+    className,
+    forceMount,
+    container,
+    size = alertDialogContentPropDefs.size.default,
+    ...contentProps
+  } = props;
+  return (
+    <AlertDialogPrimitive.Portal container={container} forceMount={forceMount}>
+      <Theme asChild>
+        <AlertDialogPrimitive.Overlay className="rt-DialogOverlay rt-AlertDialogOverlay">
+          <AlertDialogPrimitive.Content
+            {...contentProps}
+            ref={forwardedRef}
+            className={classNames(
+              'rt-DialogContent',
+              'rt-AlertDialogContent',
+              className,
+              withBreakpoints(size, 'rt-r-size'),
+            )}
+          />
+        </AlertDialogPrimitive.Overlay>
+      </Theme>
+    </AlertDialogPrimitive.Portal>
+  );
+});
 AlertDialogContent.displayName = 'AlertDialogContent';
 
 type AlertDialogTitleElement = React.ElementRef<typeof Heading>;
 type AlertDialogTitleProps = React.ComponentPropsWithoutRef<typeof Heading>;
-const AlertDialogTitle = React.forwardRef<AlertDialogTitleElement, AlertDialogTitleProps>(
-  (props, forwardedRef) => (
-    <AlertDialogPrimitive.Title asChild>
-      <Heading size="5" mb="3" trim="start" {...props} ref={forwardedRef} />
-    </AlertDialogPrimitive.Title>
-  )
-);
+const AlertDialogTitle = React.forwardRef<
+  AlertDialogTitleElement,
+  AlertDialogTitleProps
+>((props, forwardedRef) => (
+  <AlertDialogPrimitive.Title asChild>
+    <Heading size="5" mb="3" trim="start" {...props} ref={forwardedRef} />
+  </AlertDialogPrimitive.Title>
+));
 AlertDialogTitle.displayName = 'AlertDialogTitle';
 
 type AlertDialogDescriptionElement = HTMLParagraphElement;
@@ -87,20 +106,36 @@ const AlertDialogDescription = React.forwardRef<
 ));
 AlertDialogDescription.displayName = 'AlertDialogDescription';
 
-type AlertDialogActionElement = React.ElementRef<typeof AlertDialogPrimitive.Action>;
+type AlertDialogActionElement = React.ElementRef<
+  typeof AlertDialogPrimitive.Action
+>;
 interface AlertDialogActionProps
-  extends Omit<React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Action>, 'asChild'> {}
-const AlertDialogAction = React.forwardRef<AlertDialogActionElement, AlertDialogActionProps>(
-  (props, forwardedRef) => <AlertDialogPrimitive.Action {...props} ref={forwardedRef} asChild />
-);
+  extends Omit<
+    React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Action>,
+    'asChild'
+  > {}
+const AlertDialogAction = React.forwardRef<
+  AlertDialogActionElement,
+  AlertDialogActionProps
+>((props, forwardedRef) => (
+  <AlertDialogPrimitive.Action {...props} ref={forwardedRef} asChild />
+));
 AlertDialogAction.displayName = 'AlertDialogAction';
 
-type AlertDialogCancelElement = React.ElementRef<typeof AlertDialogPrimitive.Cancel>;
+type AlertDialogCancelElement = React.ElementRef<
+  typeof AlertDialogPrimitive.Cancel
+>;
 interface AlertDialogCancelProps
-  extends Omit<React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Cancel>, 'asChild'> {}
-const AlertDialogCancel = React.forwardRef<AlertDialogCancelElement, AlertDialogCancelProps>(
-  (props, forwardedRef) => <AlertDialogPrimitive.Cancel {...props} ref={forwardedRef} asChild />
-);
+  extends Omit<
+    React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Cancel>,
+    'asChild'
+  > {}
+const AlertDialogCancel = React.forwardRef<
+  AlertDialogCancelElement,
+  AlertDialogCancelProps
+>((props, forwardedRef) => (
+  <AlertDialogPrimitive.Cancel {...props} ref={forwardedRef} asChild />
+));
 AlertDialogCancel.displayName = 'AlertDialogCancel';
 
 const AlertDialog = Object.assign(
@@ -113,26 +148,26 @@ const AlertDialog = Object.assign(
     Description: AlertDialogDescription,
     Action: AlertDialogAction,
     Cancel: AlertDialogCancel,
-  }
+  },
 );
 
 export {
   AlertDialog,
-  AlertDialogRoot,
-  AlertDialogTrigger,
-  AlertDialogContent,
-  AlertDialogTitle,
-  AlertDialogDescription,
   AlertDialogAction,
   AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogRoot,
+  AlertDialogTitle,
+  AlertDialogTrigger,
 };
 
 export type {
-  AlertDialogRootProps,
-  AlertDialogTriggerProps,
-  AlertDialogContentProps,
-  AlertDialogTitleProps,
-  AlertDialogDescriptionProps,
   AlertDialogActionProps,
   AlertDialogCancelProps,
+  AlertDialogContentProps,
+  AlertDialogDescriptionProps,
+  AlertDialogRootProps,
+  AlertDialogTitleProps,
+  AlertDialogTriggerProps,
 };
