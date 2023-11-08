919ab28b4ad635e5e869bda086d2d135db0e938c
Artur Bien <artur.bien@gmail.com>
2023-11-08 13:56:04 +0100

    fix playground build error

--- apps/playground/app/snapshot/image-card.tsx
+++ apps/playground/app/snapshot/image-card.tsx
@@ -1,13 +1,22 @@
 'use client';
 
 import { DownloadIcon, HeartFilledIcon, PlusIcon } from '@radix-ui/react-icons';
-import { Button, IconButton, Popover, Text, Tooltip } from '@whop/frosted-ui';
+import {
+  Button,
+  Flex,
+  IconButton,
+  Popover,
+  RadioGroupItem,
+  RadioGroupRoot,
+  Text,
+} from '@whop/frosted-ui';
 import * as React from 'react';
 import styles from './page.module.css';
-
+// TODO: tooltips not working
 function ImageCard({ id }: { id: string | number }) {
   const [open, setOpen] = React.useState(false);
   return (
+    // TODO: why this has to be wrapped to prevent Tooltip.Provider error?
     <div className={styles.imageCard}>
       <img
         src={`https://images.unsplash.com/photo-${id}?w=400&h=400&dpr=2&auto=format&fit=crop&w=2574&q=70`}
@@ -29,39 +38,33 @@ function ImageCard({ id }: { id: string | number }) {
             right: 'var(--space-3)',
           }}
         >
-          <Tooltip content="Add to favorites">
+          <IconButton
+            size="3"
+            highContrast
+            radius="full"
+            style={{ marginRight: 'var(--space-3)' }}
+          >
+            <HeartFilledIcon />
+          </IconButton>
+          <IconButton size="3" highContrast radius="full">
+            <PlusIcon />
+          </IconButton>
+        </div>
+        <Popover.Root open={open} onOpenChange={setOpen}>
+          <Popover.Trigger>
             <IconButton
               size="3"
               highContrast
               radius="full"
-              style={{ marginRight: 'var(--space-3)' }}
+              style={{
+                position: 'absolute',
+                bottom: 'var(--space-3)',
+                right: 'var(--space-3)',
+              }}
             >
-              <HeartFilledIcon />
+              <DownloadIcon />
             </IconButton>
-          </Tooltip>
-          <Tooltip content="Add to library">
-            <IconButton size="3" highContrast radius="full">
-              <PlusIcon />
-            </IconButton>
-          </Tooltip>
-        </div>
-        <Popover.Root open={open} onOpenChange={setOpen}>
-          <Tooltip content="Download">
-            <Popover.Trigger>
-              <IconButton
-                size="3"
-                highContrast
-                radius="full"
-                style={{
-                  position: 'absolute',
-                  bottom: 'var(--space-3)',
-                  right: 'var(--space-3)',
-                }}
-              >
-                <DownloadIcon />
-              </IconButton>
-            </Popover.Trigger>
-          </Tooltip>
+          </Popover.Trigger>
           <Popover.Content align="center" className={styles.downloadPopover}>
             <div
               style={{
@@ -73,38 +76,20 @@ function ImageCard({ id }: { id: string | number }) {
               <Text as="p" size="3" style={{ marginBottom: 'var(--space-2)' }}>
                 Choose a size:
               </Text>
-
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  gap: 'var(--space-1)',
-                  marginBottom: 'var(--space-3)',
-                }}
-              >
-                {['Small', 'Medium', 'Large'].map((size) => (
-                  <label
-                    key={size}
-                    style={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      gap: 'var(--space-1)',
-                    }}
-                  >
-                    <input
-                      type="radio"
-                      name="size"
-                      value={size}
-                      defaultChecked
-                      style={{ marginTop: -1 }}
-                    />
-                    <Text size="2">{size}</Text>
-                  </label>
-                ))}
-              </div>
+              <RadioGroupRoot defaultValue="Small" mb="4">
+                <Flex gap="2" direction="column">
+                  {['Small', 'Medium', 'Large'].map((size) => (
+                    <Text as="label" size="2" key={size}>
+                      <Flex gap="2">
+                        <RadioGroupItem value={size} /> {size}
+                      </Flex>
+                    </Text>
+                  ))}
+                </Flex>
+              </RadioGroupRoot>
 
               <Popover.Close>
-                <Button size="2">
+                <Button size="2" variant="soft">
                   Download <DownloadIcon />
                 </Button>
               </Popover.Close>
