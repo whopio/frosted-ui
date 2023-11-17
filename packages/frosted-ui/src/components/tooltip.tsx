7b3d0d800d45841ef7a32b5361c9fe4e1d43e28a
Artur Bien <artur.bien@gmail.com>
2023-11-17 12:33:53 +0100

    restyle Tooltip

--- packages/frosted-ui/src/components/tooltip.tsx
+++ packages/frosted-ui/src/components/tooltip.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import * as React from 'react';
-import classNames from 'classnames';
 import * as TooltipPrimitive from '@radix-ui/react-tooltip';
+import classNames from 'classnames';
+import * as React from 'react';
+import { Theme } from '../theme';
 import { Text } from './text';
 import { tooltipPropDefs } from './tooltip.props';
-import { Theme } from '../theme';
 
 import type { GetPropDefTypes } from '../helpers';
 
@@ -13,49 +13,60 @@ type TooltipElement = React.ElementRef<typeof TooltipPrimitive.Content>;
 type TooltipOwnProps = GetPropDefTypes<typeof tooltipPropDefs>;
 interface TooltipProps
   extends React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Root>,
-    Omit<React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>, 'content'>,
+    Omit<
+      React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>,
+      'content'
+    >,
     TooltipOwnProps {
   // TODO: See if we can automate making prop defs with `required: true` non nullable
   content: NonNullable<TooltipOwnProps['content']>;
   container?: React.ComponentProps<typeof TooltipPrimitive.Portal>['container'];
 }
-const Tooltip = React.forwardRef<TooltipElement, TooltipProps>((props, forwardedRef) => {
-  const {
-    children,
-    className,
-    open,
-    defaultOpen,
-    onOpenChange,
-    delayDuration,
-    disableHoverableContent,
-    content,
-    container,
-    forceMount,
-    ...tooltipContentProps
-  } = props;
-  const rootProps = { open, defaultOpen, onOpenChange, delayDuration, disableHoverableContent };
-  return (
-    <TooltipPrimitive.Root {...rootProps}>
-      <TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
-      <TooltipPrimitive.Portal container={container} forceMount={forceMount}>
-        <Theme asChild>
-          <TooltipPrimitive.Content
-            sideOffset={4}
-            collisionPadding={10}
-            {...tooltipContentProps}
-            ref={forwardedRef}
-            className={classNames('rt-TooltipContent', className)}
-          >
-            <Text as="p" className="rt-TooltipText" size="1">
-              {content}
-            </Text>
-            <TooltipPrimitive.Arrow className="rt-TooltipArrow" />
-          </TooltipPrimitive.Content>
-        </Theme>
-      </TooltipPrimitive.Portal>
-    </TooltipPrimitive.Root>
-  );
-});
+const Tooltip = React.forwardRef<TooltipElement, TooltipProps>(
+  (props, forwardedRef) => {
+    const {
+      children,
+      className,
+      open,
+      defaultOpen,
+      onOpenChange,
+      delayDuration,
+      disableHoverableContent,
+      content,
+      container,
+      forceMount,
+      ...tooltipContentProps
+    } = props;
+    const rootProps = {
+      open,
+      defaultOpen,
+      onOpenChange,
+      delayDuration,
+      disableHoverableContent,
+    };
+    return (
+      <TooltipPrimitive.Root {...rootProps}>
+        <TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
+        <TooltipPrimitive.Portal container={container} forceMount={forceMount}>
+          <Theme asChild>
+            <TooltipPrimitive.Content
+              sideOffset={4}
+              collisionPadding={10}
+              {...tooltipContentProps}
+              ref={forwardedRef}
+              className={classNames('rt-TooltipContent', className)}
+            >
+              <Text as="p" className="rt-TooltipText" size="1">
+                {content}
+              </Text>
+              <TooltipPrimitive.Arrow className="rt-TooltipArrow" />
+            </TooltipPrimitive.Content>
+          </Theme>
+        </TooltipPrimitive.Portal>
+      </TooltipPrimitive.Root>
+    );
+  },
+);
 Tooltip.displayName = 'Tooltip';
 
 export { Tooltip };
