a2e14300f1691025ea4e9a3ba72f80d37325b7c2
Jordan Jantschulev <j.jantschulev@gmail.com>
2023-12-11 15:53:20 -0500

    feat: allow controlling Frosted theme with CustomEvents (#42)
    - temporary idea
    - Cleaned up PR
    
    ---------
    
    Co-authored-by: Artur Bień <artur.bien@gmail.com>

---
+++ packages/frosted-ui/src/use-theme-events.ts
@@ -0,0 +1,125 @@
+'use client';
+
+import React, { FC, useEffect } from 'react';
+import { useThemeContext } from './theme';
+import { ThemeOptions, themePropDefs } from './theme-options';
+
+function validateThemeColor<
+  K extends Exclude<keyof typeof themePropDefs, 'hasBackground'>,
+>(
+  key: K,
+  value: unknown,
+): value is (typeof themePropDefs)[K]['values'][number] {
+  const validValues = themePropDefs[key].values;
+  return validValues.includes(value as never);
+}
+
+function validateThemeOptions(detail: unknown) {
+  if (typeof detail !== 'object' || !detail) return {};
+
+  const ret: Partial<ThemeOptions> = {};
+
+  const keysToCheck: readonly (keyof ThemeOptions)[] = [
+    'accentColor',
+    'appearance',
+    'dangerColor',
+    'grayColor',
+    'infoColor',
+    'successColor',
+    'warningColor',
+  ];
+
+  for (const key of keysToCheck) {
+    if (!(key in detail)) continue;
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    if (!validateThemeColor(key, (detail as any)[key])) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      console.warn(`Invalid value for ${key}: ${(detail as any)[key]}`);
+      continue;
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ret[key] = (detail as any)[key];
+  }
+
+  return ret;
+}
+
+export function useThemeEvents() {
+  const {
+    accentColor,
+    appearance,
+    dangerColor,
+    grayColor,
+    infoColor,
+    successColor,
+    warningColor,
+    onAccentColorChange,
+    onAppearanceChange,
+    onDangerColorChange,
+    onGrayColorChange,
+    onInfoColorChange,
+    onSuccessColorChange,
+    onWarningColorChange,
+  } = useThemeContext();
+
+  // Emit an event when the component is mounted.
+  // Listen for theme changes from the outside.
+  useEffect(() => {
+    const listener = (e: Event) => {
+      if (e instanceof CustomEvent) {
+        const d = validateThemeOptions(e.detail);
+        if (d.appearance) onAppearanceChange(d.appearance);
+        if (d.accentColor) onAccentColorChange(d.accentColor);
+        if (d.grayColor) onGrayColorChange(d.grayColor);
+        if (d.infoColor) onInfoColorChange(d.infoColor);
+        if (d.successColor) onSuccessColorChange(d.successColor);
+        if (d.warningColor) onWarningColorChange(d.warningColor);
+        if (d.dangerColor) onDangerColorChange(d.dangerColor);
+      }
+    };
+    document.documentElement.addEventListener('frosted-ui:set-theme', listener);
+
+    // Let external code know that the component is mounted.
+    const event = new CustomEvent('frosted-ui:mounted');
+    document.documentElement.dispatchEvent(event);
+
+    return () => {
+      document.documentElement.removeEventListener(
+        'frosted-ui:set-theme',
+        listener,
+      );
+      const event = new CustomEvent('frosted-ui:unmounted');
+      document.documentElement.dispatchEvent(event);
+    };
+  }, []);
+
+  // Emit an event when the theme is changed.
+  React.useEffect(() => {
+    const event = new CustomEvent('frosted-ui:on-theme-change', {
+      detail: {
+        appearance,
+        accentColor,
+        grayColor,
+        infoColor,
+        successColor,
+        warningColor,
+        dangerColor,
+      },
+    });
+    document.documentElement.dispatchEvent(event);
+  }, [
+    appearance,
+    accentColor,
+    grayColor,
+    infoColor,
+    successColor,
+    warningColor,
+    dangerColor,
+  ]);
+}
+
+export const WithThemeEvents: FC = () => {
+  useThemeEvents();
+  return null;
+};
