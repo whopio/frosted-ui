4d5484f1f7e53d461a18355d858c54a6db25aa61
Artur Bień <artur.bien@gmail.com>
2024-03-02 21:26:37 +0100

    chore: update icons (#98)

--- packages/frosted-ui-icons/src/DollarCircle32.tsx
+++ packages/frosted-ui-icons/src/DollarCircle32.tsx
@@ -15,23 +15,7 @@ export const DollarCircle32 = React.forwardRef<SVGSVGElement, IconProps>(
         ref={forwardedRef}
       >
         <path
-          d="M11.75 22.25H18.125C19.8509 22.25 21.25 20.8509 21.25 19.125V19.125C21.25 17.3991 19.8509 16 18.125 16H13.875C12.1491 16 10.75 14.6009 10.75 12.875V12.875C10.75 11.1491 12.1491 9.75 13.875 9.75H20.25"
-          stroke={color}
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <circle
-          cx="16"
-          cy="16"
-          r="14.25"
-          stroke={color}
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M16 6.75V25.25"
+          d="M11.75 22.25H18.125C19.8509 22.25 21.25 20.8509 21.25 19.125C21.25 17.3991 19.8509 16 18.125 16H13.875C12.1491 16 10.75 14.6009 10.75 12.875C10.75 11.1491 12.1491 9.75 13.875 9.75H20.25M16 25.25V6.75M30.25 16C30.25 23.8701 23.8701 30.25 16 30.25C8.12994 30.25 1.75 23.8701 1.75 16C1.75 8.12994 8.12994 1.75 16 1.75C23.8701 1.75 30.25 8.12994 30.25 16Z"
           stroke={color}
           strokeWidth="1.5"
           strokeLinecap="round"
