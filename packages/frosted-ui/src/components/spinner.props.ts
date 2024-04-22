7e50144c7a6b12703556bd9dac2e2cd1a5e5dd6a
Artur Bien <artur.bien@gmail.com>
2024-04-22 20:48:06 +0200

    feat(Spinner): add Spinner component

---
+++ packages/frosted-ui/src/components/spinner.props.ts
@@ -0,0 +1,13 @@
+import { PropDef } from '../helpers';
+
+const sizes = ['1', '2', '3', '4', '5', '6'] as const;
+
+const spinnerPropDefs = {
+  size: { type: 'enum', values: sizes, default: '2', responsive: true },
+  loading: { type: 'boolean', default: true },
+} satisfies {
+  size: PropDef<(typeof sizes)[number]>;
+  loading: PropDef<boolean>;
+};
+
+export { spinnerPropDefs };
---
+++ packages/frosted-ui/src/components/spinner.tsx
@@ -0,0 +1,95 @@
+import classNames from 'classnames';
+import * as React from 'react';
+import {
+  extractMarginProps,
+  withBreakpoints,
+  withMarginProps,
+} from '../helpers/index.js';
+import { Flex } from './flex.js';
+import { spinnerPropDefs } from './spinner.props';
+
+import type {
+  GetPropDefTypes,
+  MarginProps,
+  PropsWithoutRefOrColor,
+} from '../helpers/index.js';
+
+type SpinnerElement = React.ElementRef<'span'>;
+type SpinnerOwnProps = GetPropDefTypes<typeof spinnerPropDefs>;
+interface SpinnerProps
+  extends PropsWithoutRefOrColor<'span'>,
+    MarginProps,
+    SpinnerOwnProps {}
+const Spinner = React.forwardRef<SpinnerElement, SpinnerProps>(
+  (props, forwardedRef) => {
+    const { rest: marginRest, ...marginProps } = extractMarginProps(props);
+
+    const {
+      children,
+      className,
+      loading = spinnerPropDefs.loading.default,
+      size = spinnerPropDefs.size.default,
+      ...spinnerProps
+    } = marginRest;
+
+    if (!loading) return children;
+
+    const spinner = (
+      <span
+        {...spinnerProps}
+        ref={forwardedRef}
+        className={classNames(
+          'fui-Spinner',
+          withBreakpoints(size, 'fui-r-size'),
+          withMarginProps(marginProps),
+          className,
+        )}
+      >
+        <span className="fui-SpinnerLeaf" />
+        <span className="fui-SpinnerLeaf" />
+        <span className="fui-SpinnerLeaf" />
+        <span className="fui-SpinnerLeaf" />
+        <span className="fui-SpinnerLeaf" />
+        <span className="fui-SpinnerLeaf" />
+        <span className="fui-SpinnerLeaf" />
+        <span className="fui-SpinnerLeaf" />
+      </span>
+    );
+
+    if (children === undefined) return spinner;
+
+    return (
+      <Flex asChild position="relative" align="center" justify="center">
+        <span>
+          {/**
+           * `display: contents` removes the content from the accessibility tree in some browsers,
+           * so we force remove it with `aria-hidden`
+           */}
+          <span
+            aria-hidden
+            style={{ display: 'contents', visibility: 'hidden' }}
+            // Workaround to use `inert` until https://github.com/facebook/react/pull/24730 is merged.
+            // eslint-disable-next-line no-constant-condition
+            {...{ inert: true ? '' : undefined }}
+          >
+            {children}
+          </span>
+
+          <Flex
+            asChild
+            align="center"
+            justify="center"
+            position="absolute"
+            inset="0"
+          >
+            <span>{spinner}</span>
+          </Flex>
+        </span>
+      </Flex>
+    );
+  },
+);
+Spinner.displayName = 'Spinner';
+
+export { Spinner };
+export type { SpinnerProps };
