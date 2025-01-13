712e7880f3d6380efc2505f3c50e041e989d5c01
Artur Bień <artur.bien@gmail.com>
2025-01-13 21:41:19 +0100

    silence eslint

--- packages/frosted-ui-colors/scripts/build-css-modules.js
+++ packages/frosted-ui-colors/scripts/build-css-modules.js
@@ -1,62 +1,64 @@
-const fs = require("fs");
-const path = require("path");
-const allColorScales = require("../index");
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require('fs');
+const path = require('path');
+const allColorScales = require('../index');
 
-const outputDir = require("../tsconfig.json").compilerOptions.outDir;
+const outputDir = require('../tsconfig.json').compilerOptions.outDir;
 
-const supportsP3AtRule = "@supports (color: color(display-p3 1 1 1))";
-const matchesP3MediaRule = "@media (color-gamut: p3)";
+const supportsP3AtRule = '@supports (color: color(display-p3 1 1 1))';
+const matchesP3MediaRule = '@media (color-gamut: p3)';
 
 Object.keys(allColorScales)
-  .filter((key) => !key.includes("P3"))
+  .filter((key) => !key.includes('P3'))
   .forEach((key) => {
-    let selector = ":root, .light, .light-theme";
+    let selector = ':root, .light, .light-theme';
 
-    if (key === "blackA" || key === "whiteA") {
-      selector = ":root";
+    if (key === 'blackA' || key === 'whiteA') {
+      selector = ':root';
     }
 
-    if (key.includes("Dark")) {
-      selector = ".dark, .dark-theme";
+    if (key.includes('Dark')) {
+      selector = '.dark, .dark-theme';
     }
 
     const srgbValues = Object.entries(allColorScales).find(
-      ([name]) => name === key
+      ([name]) => name === key,
     )[1];
 
     const srgbCssProperties = Object.entries(srgbValues)
       .map(([name, value]) => [toCssCasing(name), value])
       .map(([name, value]) => `  --${name}: ${value};`)
-      .join("\n");
+      .join('\n');
 
     const srgbCssRule = `${selector} {\n${srgbCssProperties}\n}`;
 
     const p3Values = Object.entries(allColorScales).find(
-      ([name]) => name === key + "P3" || name === key.replace(/.$/, "P3A")
+      ([name]) => name === key + 'P3' || name === key.replace(/.$/, 'P3A'),
     )[1];
 
     const p3CssProperties = Object.entries(p3Values)
       .map(([name, value]) => [toCssCasing(name), value])
       .map(([name, value]) => `      --${name}: ${value};`)
-      .join("\n");
+      .join('\n');
 
     let p3CssRule = `    ${selector} {\n${p3CssProperties}\n    }`;
     p3CssRule = `  ${matchesP3MediaRule} {\n${p3CssRule}\n  }`;
     p3CssRule = `${supportsP3AtRule} {\n${p3CssRule}\n}`;
 
     fs.writeFileSync(
-      path.join(outputDir, toFileName(key) + ".css"),
-      `${srgbCssRule}\n\n${p3CssRule}`
+      path.join(outputDir, toFileName(key) + '.css'),
+      `${srgbCssRule}\n\n${p3CssRule}`,
     );
   });
 
 function toCssCasing(str) {
   return str
-    .replace(/([a-z])(\d)/, "$1-$2")
-    .replace(/([A-Z])/g, "-$1")
+    .replace(/([a-z])(\d)/, '$1-$2')
+    .replace(/([A-Z])/g, '-$1')
     .toLowerCase();
 }
 
 function toFileName(str) {
-  return toCssCasing(str).replace(/-a$/, "-alpha");
+  return toCssCasing(str).replace(/-a$/, '-alpha');
 }
