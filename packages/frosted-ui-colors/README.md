fa3fbd0a175b315adab2b928887331b33ba0a5b5
Artur Bień <artur.bien@gmail.com>
2025-01-13 21:05:03 +0100

    scaffold

---
+++ packages/frosted-ui-colors/README.md
@@ -0,0 +1,17 @@
+# Frosted UI Colors
+
+**A gorgeous, accessible color system used by Whop.**
+
+---
+
+## Documentation
+
+For full documentation, visit [storybook.whop.dev](https://storybook.whop.dev/?path=/docs/guides-3-color--docs).
+
+## Installation
+
+`pnpm add @frosted-ui/colors`
+
+## Acknowledgments
+
+Frosted UI is heavily based on [Radix Colors](https://www.radix-ui.com/colors).
---
+++ packages/frosted-ui-colors/scripts/build-css-modules.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const path = require("path");
+const allColorScales = require("../index");
+
+const outputDir = require("../tsconfig.json").compilerOptions.outDir;
+
+const supportsP3AtRule = "@supports (color: color(display-p3 1 1 1))";
+const matchesP3MediaRule = "@media (color-gamut: p3)";
+
+Object.keys(allColorScales)
+  .filter((key) => !key.includes("P3"))
+  .forEach((key) => {
+    let selector = ":root, .light, .light-theme";
+
+    if (key === "blackA" || key === "whiteA") {
+      selector = ":root";
+    }
+
+    if (key.includes("Dark")) {
+      selector = ".dark, .dark-theme";
+    }
+
+    const srgbValues = Object.entries(allColorScales).find(
+      ([name]) => name === key
+    )[1];
+
+    const srgbCssProperties = Object.entries(srgbValues)
+      .map(([name, value]) => [toCssCasing(name), value])
+      .map(([name, value]) => `  --${name}: ${value};`)
+      .join("\n");
+
+    const srgbCssRule = `${selector} {\n${srgbCssProperties}\n}`;
+
+    const p3Values = Object.entries(allColorScales).find(
+      ([name]) => name === key + "P3" || name === key.replace(/.$/, "P3A")
+    )[1];
+
+    const p3CssProperties = Object.entries(p3Values)
+      .map(([name, value]) => [toCssCasing(name), value])
+      .map(([name, value]) => `      --${name}: ${value};`)
+      .join("\n");
+
+    let p3CssRule = `    ${selector} {\n${p3CssProperties}\n    }`;
+    p3CssRule = `  ${matchesP3MediaRule} {\n${p3CssRule}\n  }`;
+    p3CssRule = `${supportsP3AtRule} {\n${p3CssRule}\n}`;
+
+    fs.writeFileSync(
+      path.join(outputDir, toFileName(key) + ".css"),
+      `${srgbCssRule}\n\n${p3CssRule}`
+    );
+  });
+
+function toCssCasing(str) {
+  return str
+    .replace(/([a-z])(\d)/, "$1-$2")
+    .replace(/([A-Z])/g, "-$1")
+    .toLowerCase();
+}
+
+function toFileName(str) {
+  return toCssCasing(str).replace(/-a$/, "-alpha");
+}
