418345f5aa4295a11397861f4e1e2a807a10aaad
Artur Bień <artur.bien@gmail.com>
2025-07-01 12:43:31 +0200

    fix(slider): leave only one variant

--- packages/frosted-ui/src/components/slider/slider.props.ts
+++ packages/frosted-ui/src/components/slider/slider.props.ts
@@ -2,16 +2,13 @@ import type { PropDef } from '../../helpers';
 import { colorProp, highContrastProp } from '../../helpers';
 
 const sizes = ['1', '2', '3'] as const;
-const variants = ['classic', 'surface', 'soft'] as const;
 
 const sliderPropDefs = {
   size: { type: 'enum', values: sizes, default: '2' },
-  variant: { type: 'enum', values: variants, default: 'surface' },
   color: colorProp,
   highContrast: highContrastProp,
 } satisfies {
   size: PropDef<(typeof sizes)[number]>;
-  variant: PropDef<(typeof variants)[number]>;
   color: typeof colorProp;
   highContrast: typeof highContrastProp;
 };
