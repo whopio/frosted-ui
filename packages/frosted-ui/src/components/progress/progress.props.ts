aa36526a8d89c2579e1d7a3f6e63670d41efb5ed
Artur Bień <artur.bien@gmail.com>
2025-07-07 11:40:31 +0200

    fix(progress): default size

--- packages/frosted-ui/src/components/progress/progress.props.ts
+++ packages/frosted-ui/src/components/progress/progress.props.ts
@@ -4,7 +4,7 @@ import { colorProp, highContrastProp } from '../../helpers';
 const sizes = ['1', '2', '3', '4', '5', '6'] as const;
 
 const progressPropDefs = {
-  size: { type: 'enum', values: sizes, default: '3' },
+  size: { type: 'enum', values: sizes, default: '6' },
   color: { ...colorProp, default: undefined },
   highContrast: highContrastProp,
 } satisfies {
