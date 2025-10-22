b76ca37b9f0c72b1e167ffa757779fdfe83bcf32
Artur Bień <artur.bien@gmail.com>
2025-10-22 14:00:40 +0200

    feat(avatar): add size 0

--- packages/frosted-ui/src/components/avatar/avatar.props.ts
+++ packages/frosted-ui/src/components/avatar/avatar.props.ts
@@ -1,7 +1,7 @@
 import type { PropDef } from '../../helpers';
 import { colorProp, highContrastProp } from '../../helpers';
 
-const sizes = ['1', '2', '3', '4', '5', '6', '7', '8', '9'] as const;
+const sizes = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9'] as const;
 const shapes = ['circle', 'square'] as const;
 
 const avatarPropDefs = {
