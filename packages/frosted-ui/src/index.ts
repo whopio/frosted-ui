5b10eafa357befd9dfea0e7df4884a35bd4d0b0d
Artur Bień <artur.bien@gmail.com>
2025-11-22 13:15:53 +0100

    dont export reversed theme

--- packages/frosted-ui/src/index.ts
+++ packages/frosted-ui/src/index.ts
@@ -1,6 +1,6 @@
 export * from './components';
 export * from './helpers';
 export * from './tailwind-plugin';
-export { ReversedTheme, Theme, updateThemeAppearanceClass, useThemeContext } from './theme';
+export { Theme, updateThemeAppearanceClass, useThemeContext } from './theme';
 export * from './theme-options';
 export { ThemePanel } from './theme-panel';
