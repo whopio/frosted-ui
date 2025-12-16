855abe0c50467739cbb9720eb43421b2704f02f8
Artur Bień <artur.bien@gmail.com>
2025-12-16 23:29:09 +0100

    fix build

--- packages/frosted-ui-react-native/src/components/native-only-animated-view.tsx
+++ packages/frosted-ui-react-native/src/components/native-only-animated-view.tsx
@@ -1,5 +1,8 @@
+import React from 'react';
 import { Platform } from 'react-native';
 import Animated from 'react-native-reanimated';
+
+type AnimatedViewProps = React.ComponentProps<typeof Animated.View>;
 
 /**
  * This component is used to wrap animated views that should only be animated on native.
@@ -10,14 +13,15 @@ import Animated from 'react-native-reanimated';
  *   <Text>I am only animated on native</Text>
  * </NativeOnlyAnimatedView>
  */
-function NativeOnlyAnimatedView(
-  props: React.ComponentProps<typeof Animated.View> & React.RefAttributes<Animated.View>
-) {
-  if (Platform.OS === 'web') {
-    return <>{props.children as React.ReactNode}</>;
-  } else {
-    return <Animated.View {...props} />;
+const NativeOnlyAnimatedView = React.forwardRef<any, AnimatedViewProps>(
+  ({ children, ...rest }, ref) => {
+    if (Platform.OS === 'web') {
+      return <>{children as React.ReactNode}</>;
+    }
+
+    // Animated.View expects its own ref type; cast to satisfy differing React typings.
+    return <Animated.View ref={ref as any} {...rest} />;
   }
-}
+);
 
 export { NativeOnlyAnimatedView };
