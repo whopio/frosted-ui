57728e184180567fe21fa5de86c2c2bc7e793d00
Artur Bień <artur.bien@gmail.com>
2025-12-17 18:05:12 +0100

    fixup

--- packages/frosted-ui-react-native/metro.config.js
+++ packages/frosted-ui-react-native/metro.config.js
@@ -1,3 +1,5 @@
+/* eslint-env node */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
