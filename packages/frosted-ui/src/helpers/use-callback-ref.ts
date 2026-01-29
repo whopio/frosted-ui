5afc0f88e72c86692693a6cfa4b2503feb0292b1
Artur Bień <artur.bien@gmail.com>
2026-01-29 21:08:53 +0100

    f

--- packages/frosted-ui/src/helpers/use-callback-ref.ts
+++ packages/frosted-ui/src/helpers/use-callback-ref.ts
@@ -4,6 +4,7 @@ import * as React from 'react';
  * A custom hook that converts a callback to a ref to avoid triggering re-renders when passed as a
  * prop or avoid re-executing effects when passed as a dependency
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function useCallbackRef<T extends (...args: any[]) => any>(callback: T | undefined): T {
   const callbackRef = React.useRef(callback);
 
