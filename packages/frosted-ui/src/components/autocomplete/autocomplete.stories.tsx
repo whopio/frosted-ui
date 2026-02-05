88af424a760ab21ba040ec91d98f70f8c0e63f78
Artur Bień <artur.bien@gmail.com>
2026-02-05 15:34:36 +0100

    fix icons

--- packages/frosted-ui/src/components/autocomplete/autocomplete.stories.tsx
+++ packages/frosted-ui/src/components/autocomplete/autocomplete.stories.tsx
@@ -262,26 +262,6 @@ export const FieldColors: Story = {
 // With Slot
 // ============================================================================
 
-function SearchIcon() {
-  return (
-    <svg
-      width="14"
-      height="14"
-      viewBox="0 0 15 15"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      style={{ color: 'var(--gray-a10)' }}
-    >
-      <path
-        d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z"
-        fill="currentColor"
-        fillRule="evenodd"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
-}
-
 export const WithSlot: Story = {
   name: 'With Slot',
   render: () => {
@@ -302,7 +282,7 @@ export const WithSlot: Story = {
         <Autocomplete.Root items={countries}>
           <TextField.Root ref={textFieldRef}>
             <TextField.Slot>
-              <SearchIcon />
+              <MagnifyingGlass16 />
             </TextField.Slot>
             <Autocomplete.Input render={<TextField.Input placeholder="Search countries..." />} />
           </TextField.Root>
@@ -472,19 +452,6 @@ export const EmptyState: Story = {
 // With Clear Button
 // ============================================================================
 
-function ClearIcon() {
-  return (
-    <svg width="12" height="12" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z"
-        fill="currentColor"
-        fillRule="evenodd"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
-}
-
 export const WithClearButton: Story = {
   name: 'With Clear Button',
   render: () => {
@@ -501,13 +468,13 @@ export const WithClearButton: Story = {
         <Autocomplete.Root items={countries}>
           <TextField.Root ref={textFieldRef}>
             <TextField.Slot>
-              <SearchIcon />
+              <MagnifyingGlass16 />
             </TextField.Slot>
             <Autocomplete.Input render={<TextField.Input placeholder="Search countries..." />} />
             <TextField.Slot>
               <Autocomplete.Clear>
                 <IconButton variant="ghost" color="gray" size="1">
-                  <ClearIcon />
+                  <XCircleFilled16 />
                 </IconButton>
               </Autocomplete.Clear>
             </TextField.Slot>
