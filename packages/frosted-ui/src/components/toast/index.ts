c9071a78f9cefea88e0f312a81e4c8c527145fc8
Artur Bień <artur.bien@gmail.com>
2026-02-23 17:25:19 +0100

    remove withToastPromise

--- packages/frosted-ui/src/components/toast/index.ts
+++ packages/frosted-ui/src/components/toast/index.ts
@@ -1,5 +1,4 @@
 export * as Toast from './toast';
 export { toast } from './toast-manager';
 export type { ToastOptions, ToastPromiseOptions, ToastType } from './toast-manager';
-export { withToastPromise } from './with-toast-promise';
 export * from './toast.props';
